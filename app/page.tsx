f28525c357822db357e4a9967a9556f9bc5173b0
Ben Yang <ben.yang@utexas.edu>
2024-01-20 21:16:28 -0600

    Use TS array

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,85 @@
 import React from "react";
 import { Flex, Text, Heading, Link, Table } from "@radix-ui/themes";
 
+
+type Song = {
+  game: string
+  name: string
+  videoLink: string
+  videoStyle: string
+  sheetMusicLink: string
+  sheetMusicOrigin: string
+}
+
 export default function Home() {
+  const songs: Song[] = [
+    {
+      game: "Super Mario 64",
+      name: "Bob-omb Battlefield",
+      videoLink: "https://www.youtube.com/watch?v=LPq8_HB8QBA",
+      videoStyle: "8-Bit Big Band Cover",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/super-mario-64/main-theme?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+    {
+      game: "Zelda: Breath of the Wild",
+      name: "Hateno Village Theme",
+      videoLink: "https://www.youtube.com/watch?v=Uj07-YU5cTk",
+      videoStyle: "Original",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/the-legend-of-zelda-breath-of-the-wild/hateno-village-day?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+    {
+      game: "Zelda: The Wind Waker",
+      name: "Dragon Roost Island",
+      videoLink: "https://www.youtube.com/watch?v=QtcgZGp3FGs",
+      videoStyle: "Original",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/the-legend-of-zelda-the-wind-waker/dragon-roost-island?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+    {
+      game: "Chrono Trigger",
+      name: "Secret of the Forst",
+      videoLink: "https://www.youtube.com/watch?v=pYgQEjcosP0",
+      videoStyle: "Contraband Cover",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/chrono-trigger/secret-of-the-forest?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+    {
+      game: "N/A",
+      name: "Wii Shop Channel Theme",
+      videoLink: "https://www.youtube.com/watch?v=yyjUmv1gJEg",
+      videoStyle: "Original",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/wii/wii-shop-channel?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+    {
+      game: "Undertale",
+      name: "Once upon a time",
+      videoLink: "https://www.youtube.com/watch?v=s7RRgF5Ve_E",
+      videoStyle: "Original",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/undertale/once-upon-a-time?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+    {
+      game: "Pikmin 2",
+      name: "Perplexing Pool",
+      videoLink: "https://www.youtube.com/watch?v=gw5QPreqViE",
+      videoStyle: "8-bit Music Theory Cover",
+      sheetMusicLink: "https://drive.google.com/drive/u/0/folders/1sf3Jlr16JhZb9w9tdj2N7md6LUqZcN1l",
+      sheetMusicOrigin: "Google Drive"
+    },
+    {
+      game: "Mario Cart 64",
+      name: "Rainbow Road",
+      videoLink: "https://www.youtube.com/watch?v=E5lAfUJdzzc",
+      videoStyle: "Original",
+      sheetMusicLink: "https://www.vgleadsheets.com/view/mario-kart-64/rainbow-road?transposition=C#",
+      sheetMusicOrigin: "vgleadsheets"
+    },
+  ]
+
+
   return (
     <Flex direction="column" gap="2" p="5">
       <Heading size="8" weight="regular">
@@ -14,7 +92,11 @@ export default function Home() {
       <Heading size="6" weight="regular">
         Who
       </Heading>
-      <Text>All experience levels welcome.</Text>
+      <Text>All experience levels welcome. Recommended skills:</Text>
+        <Text ml="2">
+          <li>Can read sheet music or learn by ear.</li>
+          <li>Know basic chords.</li>
+        </Text>
       <Heading size="6" weight="regular">
         When
       </Heading>
@@ -41,118 +123,24 @@ export default function Home() {
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>Super Mario 64</Table.RowHeaderCell>
-            <Table.Cell>Bob-Omb Battlefield</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=LPq8_HB8QBA">
-                Cover
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/super-mario-64/main-theme?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Zelda: Breath of the Wild</Table.RowHeaderCell>
-            <Table.Cell>Hateno Village Theme</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=Uj07-YU5cTk">
-                Original
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/the-legend-of-zelda-breath-of-the-wild/hateno-village-day?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Zelda: The Wind Waker</Table.RowHeaderCell>
-            <Table.Cell>Dragon Roost Island</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=QtcgZGp3FGs">
-                Original
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/the-legend-of-zelda-the-wind-waker/dragon-roost-island?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Chrono Trigger</Table.RowHeaderCell>
-            <Table.Cell>Secret of the Forest</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=pYgQEjcosP0">
-                Cover
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/chrono-trigger/secret-of-the-forest?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Wii</Table.RowHeaderCell>
-            <Table.Cell>Wii Shop Channel Theme</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=yyjUmv1gJEg">
-                Original
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/wii/wii-shop-channel?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Undertale</Table.RowHeaderCell>
-            <Table.Cell>Once Upon A Time</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=s7RRgF5Ve_E">
-                Original
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/undertale/once-upon-a-time?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Pikmin 2</Table.RowHeaderCell>
-            <Table.Cell>Perplexing Pool</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=gw5QPreqViE">
-                Cover
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://drive.google.com/drive/u/0/folders/1sf3Jlr16JhZb9w9tdj2N7md6LUqZcN1l">
-                Google Drive
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Mario Cart 64</Table.RowHeaderCell>
-            <Table.Cell>Rainbow Road</Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.youtube.com/watch?v=E5lAfUJdzzc">
-                Cover
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link href="https://www.vgleadsheets.com/view/mario-kart-64/rainbow-road?transposition=C#">
-                VG Lead Sheets
-              </Link>
-            </Table.Cell>
-          </Table.Row>
+          {songs.map((song) =>
+            <Table.Row>
+              <Table.RowHeaderCell>{song.game}</Table.RowHeaderCell>
+              <Table.Cell>{song.name}</Table.Cell>
+              <Table.Cell>
+                <Link href={song.videoLink}>
+                  {song.videoStyle}
+                </Link>
+              </Table.Cell>
+              <Table.Cell>
+                <Link href={song.sheetMusicLink}>
+                  {song.sheetMusicOrigin}
+                </Link>
+              </Table.Cell>
+            </Table.Row>
+          )}
+      
+          
         </Table.Body>
       </Table.Root>
       <Heading size="6" weight="regular">
@@ -175,8 +163,7 @@ export default function Home() {
         Cost
       </Heading>
       <Text>
-        I would appreciate if you contributed $5-12 dollars to cover the room
-        cost and drum rental.
+        I would appreciate if you contributed ~$10 to cover the room cost and drum rental.
       </Text>
     </Flex>
   );

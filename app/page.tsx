29cc3fdf623d950324d529cd632a21913a17cad3
Ben Yang <ben.yang@utexas.edu>
2026-04-15 19:33:51 -0500

    add links to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Flex, Link } from "@radix-ui/themes";
 export default function Home() {
   return (
     <Flex direction="column" gap="2" p="5">
-      <Link href="./info/vgm">VGM jam</Link>
-      <Link href="./info/finalbossa">Final Bossa setlist</Link>
+      <Link href="./vgm">VGM jam</Link>
+      <Link href="./finalbossa">Final Bossa setlist</Link>
     </Flex>
   );
 }
